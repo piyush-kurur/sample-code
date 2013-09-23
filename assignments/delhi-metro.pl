6ef7e6384162383f106fa1b70cb902faf6dd80c1
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-09-23 17:45:43 +0530

    added first assignment

---
+++ assignments/delhi-metro.pl
@@ -0,0 +1,88 @@
+%% Assignment 1: Delhi Metro
+%% Due on: 30 Sep 2013 (Monday) before midnight.
+
+%% Please fill the following details
+%% Full Name:
+%% Roll No:
+
+%% Given below is the metro lines of New Delhi. Write a function
+%% that given any two metro station prints out the route to take
+
+%% For example typing
+%%     > findRoute(dwarka,new-delhi).
+%% should print
+%%     > From dwarka take blue line to rajiv-chowk
+%%     > From rajiv-chowk take yellow line to new-delhi
+
+%% Concepts not done in class: Formated print, List functions.
+%%
+
+line(red,
+	[ rithala,
+          rohini-east,
+          rohini-west,
+          pitam-pura,
+          kohat-enclave,
+          netaji-subash-place,
+          keshav-puram,
+          kanhaiya-nagar,
+          indralok,
+          shastri-nagar,
+          pratap-nagar,
+          pul-bangesh,
+          tiz-hazari,
+          kashmiri-gate,
+          shastri-park,
+          seelam-pur,
+          welcome,
+          shahdara,
+          mansarovar-park,
+          jhilmil,
+          dilshad-garden
+        ]
+    ).
+line(yellow,[ vishwa-vidayala
+            , vidhan-sabha
+            , civil-lines
+            , kashmiri-gate
+            , chandini-chowk
+            , chawari-bazar
+            , new-delhi
+            , rajiv-chowk
+            , patel-chowk
+            , central-secretariat
+            ]
+    ).
+
+line(blue,[ indraprastha
+          , pragati-maidan
+          , mandi-house
+          , barakhamba-road
+          , rajiv-chowk
+          , ramakrishna-ashram-marg
+          , jhandewala
+          , karol-bagh
+          , rajendra-place
+          , patel-nagar
+          , shadipur
+          , kirti-nagar
+          , moti-nagar
+          , ramesh-nagar
+          , rajouri-garden
+          , tagore-garden
+          , subhash-nagar
+          , tilak-nagar
+          , janakpuri-east
+          , janakpuri-west
+          , uttam-nagar-east
+          , uttam-nagar-west
+          , nawada
+          , dwarka-mor
+          , dwarka
+          , sector-14-dwarka
+          , sector-13-dwarka
+          , sector-12-dwarka
+          , sector-11-dwarka
+          , sector-10-dwarka
+          , sector-9-dwarka
+          ]).
