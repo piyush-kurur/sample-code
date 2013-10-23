6e98093377cb641039aec5ba1cc986e514cebffa
Piyush P Kurur <ppk@cse.iitk.ac.in>
2013-10-23 15:37:23 +0530

    signal light changes state on message from anyone.

--- assignments/traffic/signal.erl
+++ assignments/traffic/signal.erl
@@ -9,27 +9,27 @@
 send_mesg(Pid,Message) ->
     Pid ! {self(), Message}.
 
-switch_state(ControllerPid, State, NewState) ->
+switch_state(Pid, State, NewState) ->
     case NewState of
-	red   -> send_mesg(ControllerPid, 'now red'),
-	         signal_light(ControllerPid, red);
-	green -> send_mesg(ControllerPid, 'now green'),
-		 signal_light(ControllerPid, green);
-	error -> send_mesg(ControllerPid,
-			      {'unknown new state', NewState}),
-		 signal_light(ControllerPid, State)
+	red   ->
+	    send_mesg(Pid, 'now red'),
+	    signal_light(Pid, red);
+	green ->
+	    send_mesg(Pid, 'now green'),
+	    signal_light(Pid, green);
+	error ->
+	    send_mesg(Pid,{'unknown new state', NewState}),
+	    signal_light(Pid, State)
     end.
 
 
-signal_light(ControllerPid, State) ->
+signal_light(State) ->
     receive
-	{Pid, ask} -> send_mesg(Pid,State),
-		      signal_light(ControllerPid, State);
-	{Pid, NewState} when Pid == ControllerPid ->
-	    switch_state(ControllerPid, State, NewState);
-	{Pid, _} ->
-	    send_mesg(Pid, 'Permission Denied'),
-	    signal_light(ControllerPid, State);
+	{Pid, ask} ->
+	    send_mesg(Pid,State),
+	    signal_light(State);
+	{Pid, NewState} ->
+	    switch_state(Pid, State, NewState);
 	error ->
-	    signal_light(ControllerPid, State)
+	    signal_light(State)
     end.
